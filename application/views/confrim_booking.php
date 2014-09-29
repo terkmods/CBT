2231f164c0883e5aefe0c209e1ff2a083d4131f3
connae <connae@10.5.7.214>
2014-09-30 03:45:58 +0700

    Merge origin/master
    Conflicts:
    	application/views/mg.php

--- application/views/confrim_booking.php
+++ application/views/confrim_booking.php
@@ -1,31 +1,47 @@
 <?php include 'template/head.php';
 ?>
+
 <div class="container">
     <div class="row">
-
-
-
-        <div class="col-md-12 text-center">
-            <fieldset>
-                <legend>  การจองเสร็จสมบูรณ์</legend>
-                <p>จองสนาม : <?= $stadium[0]->stadium_name ?></p>
-                <p>คอร์ด :<?= $court->court_name ?></p>
-                <p>วันที่ : <?= $date ?>  </p>
-                <p>เวลา : <?= $start_time ?> - <?= $end_time ?>  </p>
-            </fieldset>
-            <div class="col-md-4 col-md-offset-4 text-center">
+        <div class="col-xs-12">
+            <div class="text-center">
+                <i class="fa fa-search-plus pull-left icon"></i>
+                <h2> Reserve Id # <?=$reserve_id?></h2>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-xs-12 col-md-5 col-lg-4 col-lg-offset-4">
+                    <div class="panel panel-default height">
+                        <div class="panel-heading">Reserve Details</div>
+                        <div class="panel-body">
+                            <strong>จองสนาม : <?= $stadium_send[0]->stadium_name ?></strong><br>
+                            คอร์ด :<?= $court_send->court_name ?><br>
+                            วันที่ : <?= $date ?>  <br>
+                            เวลา : <?= $start_time_send ?> - <?= $end_time_send ?> <br>
+                            ชื่อผู้จอง : <?= $user[0]->fname?>  <?= $user[0]->lname?> <br>
+                           รวมเป็นเงิน <strong><?= $sumprice?></strong><br>
+                        </div>
+                    </div>
+                    
+                </div>
+                            <div class="col-md-4 col-md-offset-4 text-center">
             <h4> โปรดอ่านข้อปฏิบัติการจองของสนาม</h4>
-            <textarea class="form-control" rows="4" cols="50"  disabled=""><?= $stadium[0]->rule ?></textarea>
+            <textarea class="form-control" rows="4" cols="50"  disabled=""><?= $stadium_send[0]->rule ?></textarea>
             <div class="checkbox text-center  " >
                 <label>
                     <input type="checkbox" id="myCheck"> ฉันได้อ่านข้อตกลง และยอมรับข้อปฏิบัติของสนาม
                 </label>
             </div>
-            <button type="button" class="next_button form-control" value="History booking" disabled="" onclick="location.href='<?=base_url()?>/booking/historybooking'">History Booking</button>
+            
+            <button type="button" class="next_button form-control" value="History booking" disabled="" onclick="location.href='<?=base_url()?>/booking/bookja'">Confrim Reserve</button>
             </div>
-        </div>
-
+                
+                
+                
+            </div>
+        
     </div>
+    
 </div>
 
 <?php include 'template/footer.php'; ?>
--- application/views/template/sideSetting.php
+++ application/views/template/sideSetting.php
@@ -2,7 +2,7 @@
 <?php $type2 = $this->uri->segment(2) ?>    
 <?php $type3 = $this->uri->segment(3) ?>    
 <div class="col-md-3">
-    <ul class="nav nav-pills nav-stacked">
+    <ul class="nav nav-pills nav-stacked nav-tabs" id="myTab">
         <?php if($this->session->userdata('role') == "owner"){ ?>
         <li <?php if($type == "users" ){?><?="class = 'active'"; }?> ><a href="<?php echo base_url() ?>users/edituser/<?php echo $this->session->userdata('id'); ?>">Basic Setting</a></li>
         <li <?php if($type == "stadium" ){?><?="class = 'active'"; }?> >
@@ -10,7 +10,10 @@
         </li>
       
         <li <?php if($type2 == "historyBooking" ){?><?="class = 'active'"; }?> ><a href="<?= base_url() ?>stadium/historyBooking">History Booking stadium </a></li>
-        <?php }else{?>
+        
+            
+            
+            <?php }else{?>
         <li <?php if($type == "users" ){?><?="class = 'active'"; }?> ><a href="<?php echo base_url() ?>users/edituser/<?php echo $this->session->userdata('id'); ?>">Basic Setting</a></li>
         <li <?php if($type == "booking" ){?><?="class = 'active'"; }?>><a href="<?= base_url() ?>booking/historybooking">History Reserve</a></li>
         <?php }?>
